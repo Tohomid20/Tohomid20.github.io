c74b21b3ac63d963a3dbccf2acb7ac31d56cc945
Tohomid Hasan <tohomid20@gmail.com>
2026-09-17 16:31:17 +0600

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "tohmid-site-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./assets/favicon.svg",
+  "./assets/profile.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
